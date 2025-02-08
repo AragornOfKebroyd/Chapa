605d6dffc5eaa1615074d993cc419fd4dd56234d
Shenglun <shenglun2000@gmail.com>
2025-02-08 21:04:43 +0000

    more baddged squibbldorb

---
+++ scripts/badger.gd
@@ -0,0 +1,49 @@
+extends CharacterBody2D
+
+var speed : float = 2.0
+var is_straying : bool = false
+var offset = 0;
+
+@onready var navigation_agent = $NavigationAgent2D
+
+
+# on collision: stops process, is_straying = true, follow_sound
+# once reached sound coordinate: wait 1 sec, is_straying = false, resume path
+
+func _on_area_2d_area_entered(area):
+	print("collision")
+
+
+# Collides with sound wave
+#func _on_Area2D_body_entered(area):
+	#print("collide")
+	#if body.has_method("on_wave_hit"):
+		#is_straying = true
+		#navigation_agent.target_position = Vector2(100, 100)
+		#set_process(true)
+
+
+
+
+
+func _ready() -> void:
+	pass
+
+
+# Called every frame
+func _process(delta):
+	# Move back onto path2d
+	if not is_straying:
+		offset += speed * delta
+		if offset > 1.0:
+			offset = 0.0  # Loop back to start when reaching the end
+	# Move to where sound came from
+	else:
+		if navigation_agent.is_navigation_finished():
+			is_straying = false
+			get_tree().create_timer(1).timeout.connect(func(): set_process(true))
+		else:
+			var next_position = navigation_agent.get_next_path_position()
+			velocity = (next_position - global_position).normalized() * speed
+			move_and_slide()
+		
