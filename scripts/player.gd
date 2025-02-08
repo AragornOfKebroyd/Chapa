5e8744334ee72f1b861135600086eeeadb61a19b
AragornOfKebroyd <benjamin.tinsley54@gmail.com>
2025-02-08 11:11:03 +0000

    added basic movement

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,23 +1,68 @@
 extends CharacterBody2D
 
-@export var SPEED = 300.0
+signal collision
+signal hit
+
+@export var SPEED = 240.0
 const JUMP_VELOCITY = -400.0
 var screen_size
-func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+func _ready():
+	screen_size = get_viewport_rect().size
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
+
+
+
+func _process(delta: float):
+	# set animation
+	if velocity.length() > 0:
+		pass
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		pass
 
-	move_and_slide()
+const mu = 0.2
+const THRESHHOLD = 7
+const ONE_OVER_ROOT_TWO = 0.70710678118
+var fMult = SPEED * SPEED * mu
+
+func _physics_process(delta: float) -> void:
+	#velocity = Vector2.ZERO
+	var force = Vector2.ZERO
+	
+	var up_down = 0
+	var left_right = 0
+	if Input.is_action_pressed("move_right"):
+		left_right += 1
+	if Input.is_action_pressed("move_left"):
+		left_right -= 1
+	if Input.is_action_pressed("move_down"):
+		up_down += 1
+	if Input.is_action_pressed("move_up"):
+		up_down -= 1
+
+	
+	if up_down == 0:
+		force.x = left_right * fMult
+	elif left_right == 0:
+		force.y = up_down * fMult
+	else:
+		force.x = left_right * fMult * 0.5
+		force.y = up_down * fMult * 0.5
+	
+	
+	# ma = Fres
+	var resistance = -sign(velocity) * mu * velocity * velocity
+	var acceleration = force + resistance
+	print("force: ", force, "\nresistance", resistance, "\nacceleration: ", acceleration,"\nvelocity: ",velocity)
+	
+	#if sign(velocity + acceleration * delta) == sign(velocity) or velocity == Vector2.ZERO:
+	velocity += acceleration * delta
+	#else:
+		#velocity = Vector2.ZERO
+	
+	if abs(velocity.x) < THRESHHOLD and sign(velocity) != sign(acceleration):
+		velocity.x = 0
+	if abs(velocity.y) < THRESHHOLD and sign(velocity) != sign(acceleration):
+		velocity.y = 0
+	
+	position += velocity * delta
