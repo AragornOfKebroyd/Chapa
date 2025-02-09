79d50831a7fd61d082b85304fb2f2c77ba8dfb6d
Shenglun <shenglun2000@gmail.com>
2025-02-09 11:49:14 +0000

    more badger moves

--- scripts/badger.gd
+++ scripts/badger.gd
@@ -1,16 +1,18 @@
 extends CharacterBody2D
 
 
-var speed = 200
+var speed = 100
 var state : String = "patrol"
 var target_position : Vector2
 var start_position : Vector2
 var targeting = false
 
 @export_enum("linear", "loop") var patrol_type = "linear"
+@export var player : CharacterBody2D 
 
 @onready var pathfollow = get_parent()
 @onready var nav_agent = $NavigationAgent2D
+#@onready var player = get_parent().get_parent().get_parent().get_node("player")
 
 func _ready() -> void:
 	nav_agent.target_desired_distance = 4.0
@@ -39,8 +41,13 @@ func target(delta):
 # badger follows player
 # then moves back to loop start
 func follow(delta):
+	var next_point = nav_agent.get_next_path_position()
+	var direction = (next_point - global_position).normalized()
 	
-	pass
+	velocity = direction * speed
+	
+	move_and_slide()
+
 
 func return_to_path(delta):
 	if nav_agent.is_navigation_finished():
@@ -52,19 +59,32 @@ func return_to_path(delta):
 	velocity = direction * speed
 	move_and_slide()
 
+
 func _physics_process(delta: float) -> void:
 	if state == "patrol":
+		start_position = global_position
 		patrol(delta)
 	if state == "target":
 		target(delta)
 	if state == "return":
 		return_to_path(delta)
+	if state == "follow":
+		nav_agent.target_position = player.global_position
+		follow(delta)
 
 func _on_object_detected_badger(sound_position):
-	print("signal recieved from position",sound_position)
-	start_position = global_position
+	#start_position = global_position
 	target_position = sound_position
 	nav_agent.target_position = target_position
 	state = "target"
 
-# to do 
+func _on_view_body_entered(body: Node2D) -> void:
+	if body.is_in_group("Player"):
+		state = "follow"
+
+
+func _on_view_body_exited(body: Node2D) -> void:
+	print(start_position)
+	if body.is_in_group("Player"):
+		nav_agent.target_position = start_position
+		state = "return"
